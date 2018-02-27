d898d64e4ae9c6ac5c27257e01f0fcf985bb58ec
lukaszgo3 <lukaszgo3@gmail.com>
2018-02-27 16:49:54 +0100

    added I/O vat and income/cost method

--- src/main/java/pl/coderstrust/taxservice/TaxCalculatorService.java
+++ src/main/java/pl/coderstrust/taxservice/TaxCalculatorService.java
@@ -6,7 +6,6 @@ import pl.coderstrust.model.Invoice;
 import pl.coderstrust.model.InvoiceEntry;
 
 import java.math.BigDecimal;
-import java.text.DecimalFormat;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,46 +13,71 @@ import java.util.List;
 public class TaxCalculatorService {
 
   private Database database;
-  private DecimalFormat decimalFormat = new DecimalFormat("#0.00");
 
   @Autowired
   public TaxCalculatorService(Database database) {
     this.database = database;
   }
 
-  public String calculateIncomeCost(String companyName,LocalDate beginDate, LocalDate endDate) {
+  public BigDecimal calculateIncome(String companyName, LocalDate beginDate, LocalDate endDate) {
     BigDecimal income = BigDecimal.valueOf(0);
-    BigDecimal cost = BigDecimal.valueOf(0);
-
     List<Invoice> invoiceByDates = new ArrayList<>(getInvoiceByDate(beginDate, endDate));
 
     for (Invoice invoice : invoiceByDates) {
       if (invoice.getSeller().getName().equals(companyName)) {
         income = income.add(getNetValue(invoice));
       }
+    }
+    return income;
+  }
+
+  public BigDecimal calculateCost(String companyName, LocalDate beginDate, LocalDate endDate) {
+    BigDecimal cost = BigDecimal.valueOf(0);
+    List<Invoice> invoiceByDates = new ArrayList<>(getInvoiceByDate(beginDate, endDate));
+
+    for (Invoice invoice : invoiceByDates) {
       if (invoice.getBuyer().getName().equals(companyName)) {
         cost = cost.add(getNetValue(invoice));
       }
     }
-
-    return decimalFormat.format(income.subtract(cost));
+    return cost;
   }
 
-  public String calculateVat(String companyName,LocalDate beginDate, LocalDate endDate) {
-    BigDecimal income = BigDecimal.valueOf(0);
-    BigDecimal cost = BigDecimal.valueOf(0);
+  public BigDecimal calculateIncomeCost(String companyName, LocalDate beginDate,
+      LocalDate endDate) {
+    return calculateIncome(companyName, beginDate, endDate)
+        .subtract(calculateCost(companyName, beginDate, endDate));
+  }
 
+  public BigDecimal calculateIncomeVat(String companyName, LocalDate beginDate, LocalDate endDate) {
+    BigDecimal incomeVat = BigDecimal.valueOf(0);
+    List<Invoice> invoiceByDates = new ArrayList<>(getInvoiceByDate(beginDate, endDate));
+
+    for (Invoice invoice : invoiceByDates) {
+      if (invoice.getBuyer().getName().equals(companyName)) {
+        incomeVat = incomeVat.add(getVatValue(invoice));
+      }
+    }
+    return incomeVat;
+  }
+
+  public BigDecimal calculateOutcomeVat(String companyName, LocalDate beginDate,
+      LocalDate endDate) {
+    BigDecimal outcomeVat = BigDecimal.valueOf(0);
     List<Invoice> invoiceByDates = new ArrayList<>(getInvoiceByDate(beginDate, endDate));
 
     for (Invoice invoice : invoiceByDates) {
       if (invoice.getSeller().getName().equals(companyName)) {
-        income = income.add(getVatValue(invoice));
-      }
-      if (invoice.getBuyer().getName().equals(companyName)) {
-        cost = cost.add(getVatValue(invoice));
+        outcomeVat = outcomeVat.add(getVatValue(invoice));
       }
     }
-    return decimalFormat.format(income.subtract(cost));
+    return outcomeVat;
+  }
+
+  public BigDecimal calculateDifferenceVat(String companyName, LocalDate beginDate,
+      LocalDate endDate) {
+    return calculateOutcomeVat(companyName, beginDate, endDate)
+        .subtract(calculateIncomeVat(companyName, beginDate, endDate));
   }
 
   private BigDecimal getNetValue(Invoice invoice) {
@@ -74,6 +98,7 @@ public class TaxCalculatorService {
     for (InvoiceEntry products : invoice.getProducts()) {
       int amount = products.getAmount();
       double vatRate = products.getProduct().getVatRate().getVatPercent();
+
       BigDecimal netValue = products.getProduct().getNetValue()
           .multiply(BigDecimal.valueOf(vatRate));
       netValueMultiplyAmount = netValueMultiplyAmount
