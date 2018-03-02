c4c80fb7e045558c67e9d353786e5bb5256af1c8
lukaszgo3 <lukaszgo3@gmail.com>
2018-03-02 01:57:33 +0100

    setScale for BigDecimal and some others

--- src/main/java/pl/coderstrust/taxservice/TaxCalculatorController.java
+++ src/main/java/pl/coderstrust/taxservice/TaxCalculatorController.java
@@ -7,12 +7,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import pl.coderstrust.model.Messages;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDate;
 
 @RestController
 @Configuration
 public class TaxCalculatorController {
+
+  //TODO change "@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)" to not repeat every time
 
   private TaxCalculatorService taxService;
 
@@ -29,7 +34,11 @@ public class TaxCalculatorController {
       @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
       @RequestParam(value = "endDate") LocalDate endDate
   ) {
-    return ResponseEntity.ok(taxService.calculateIncome(companyName, startDate, endDate));
+    if (endDate.isAfter(startDate)) {
+      return ResponseEntity.ok(taxService.calculateIncome(companyName, startDate, endDate)
+          .setScale(2, RoundingMode.HALF_UP));
+    }
+    return ResponseEntity.badRequest().body(Messages.END_BEFORE_START);
   }
 
   @RequestMapping(value = "cost")
@@ -40,19 +49,28 @@ public class TaxCalculatorController {
       @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
       @RequestParam(value = "endDate") LocalDate endDate
   ) {
-    return ResponseEntity.ok(taxService.calculateCost(companyName, startDate, endDate));
+    if (endDate.isAfter(startDate)) {
+      return ResponseEntity.ok(taxService.calculateCost(companyName, startDate, endDate)
+          .setScale(2, RoundingMode.HALF_UP));
+    }
+    return ResponseEntity.badRequest().body(Messages.END_BEFORE_START);
   }
 
-  @RequestMapping(value = "incomeCost")
-  public ResponseEntity calculateIncomeCost(
+  @RequestMapping(value = "incomeTax")
+  public ResponseEntity calculateIncomeTax(
       @RequestParam(value = "companyName") String companyName,
       @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
       @RequestParam(value = "startDate") LocalDate startDate,
       @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
       @RequestParam(value = "endDate") LocalDate endDate
   ) {
-    return ResponseEntity.ok(taxService.calculateIncome(companyName, startDate, endDate)
-        .subtract(taxService.calculateCost(companyName, startDate, endDate)));
+    if (endDate.isAfter(startDate)) {
+      BigDecimal income = taxService.calculateIncome(companyName, startDate, endDate);
+      BigDecimal cost = taxService.calculateCost(companyName, startDate, endDate);
+      return ResponseEntity.ok(income.subtract(cost).setScale(2, RoundingMode.HALF_UP));
+    }
+
+    return ResponseEntity.badRequest().body(Messages.END_BEFORE_START);
   }
 
   @RequestMapping(value = "incVat")
@@ -63,7 +81,12 @@ public class TaxCalculatorController {
       @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
       @RequestParam(value = "endDate") LocalDate endDate
   ) {
-    return ResponseEntity.ok(taxService.calculateIncomeVat(companyName, startDate, endDate));
+
+    if (endDate.isAfter(startDate)) {
+      return ResponseEntity.ok(taxService.calculateIncomeVat(companyName, startDate, endDate)
+          .setScale(2, RoundingMode.HALF_UP));
+    }
+    return ResponseEntity.badRequest().body(Messages.END_BEFORE_START);
   }
 
   @RequestMapping(value = "outVat")
@@ -74,7 +97,11 @@ public class TaxCalculatorController {
       @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
       @RequestParam(value = "endDate") LocalDate endDate
   ) {
-    return ResponseEntity.ok(taxService.calculateOutcomeVat(companyName, startDate, endDate));
+    if (endDate.isAfter(startDate)) {
+      return ResponseEntity.ok(taxService.calculateOutcomeVat(companyName, startDate, endDate)
+          .setScale(2, RoundingMode.HALF_UP));
+    }
+    return ResponseEntity.badRequest().body(Messages.END_BEFORE_START);
   }
 
   @RequestMapping(value = "diffVat")
@@ -85,7 +112,11 @@ public class TaxCalculatorController {
       @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
       @RequestParam(value = "endDate") LocalDate endDate
   ) {
-    return ResponseEntity.ok(taxService.calculateOutcomeVat(companyName, startDate, endDate)
-        .subtract(taxService.calculateIncomeVat(companyName, startDate, endDate)));
+    if (endDate.isAfter(startDate)) {
+      BigDecimal outVat = taxService.calculateOutcomeVat(companyName, startDate, endDate);
+      BigDecimal incVat = taxService.calculateIncomeVat(companyName, startDate, endDate);
+      return ResponseEntity.ok(outVat.subtract(incVat).setScale(2, RoundingMode.HALF_UP));
+    }
+    return ResponseEntity.badRequest().body(Messages.END_BEFORE_START);
   }
 }
