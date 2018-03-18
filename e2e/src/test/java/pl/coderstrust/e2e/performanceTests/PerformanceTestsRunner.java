b5593206e3d3c9915a8f21714e5ebc8f260e51b5
SebastianPal <spalewski@gmail.com>
2018-03-18 21:11:39 +0100

    PerformanceTests extends ValidInputTests

--- e2e/src/test/java/pl/coderstrust/e2e/performanceTests/PerformanceTestsRunner.java
+++ e2e/src/test/java/pl/coderstrust/e2e/performanceTests/PerformanceTestsRunner.java
@@ -1,19 +1,36 @@
 package pl.coderstrust.e2e.performanceTests;
 
 
+
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import pl.coderstrust.e2e.ValidInputTests;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-public class PerformanceTestsRunner extends PerformanceTests{
+public class PerformanceTestsRunner extends ValidInputTests{
 
-    @Test(dependsOnGroups={"PerformanceTests.run"})
+    public PerformanceTestsRunner() {
+        this.validInputTests = new ValidInputTests();
+    }
+
+    private ValidInputTests validInputTests;
+    private int THREADS_NUMBER = 10;
+
+
+    @BeforeClass
+    public void setupClass() {
+        validInputTests.setupClass();
+        validInputTests.setupMethod();
+    }
+
+    @Test
     public void shouldAddInvoicesIn5Threads() {
 
         final ExecutorService newFixedThreadPool = Executors.newFixedThreadPool(5);
-        for (int i = 0; i <5; i++) {
+        for (int i = 0; i < 5; i++) {
             newFixedThreadPool.execute(new PerformanceTests());
         }
         newFixedThreadPool.shutdown();
@@ -25,11 +42,11 @@ public class PerformanceTestsRunner extends PerformanceTests{
         newFixedThreadPool.shutdown();
     }
 
-    @Test(dependsOnGroups={"PerformanceTests.run"})
+    @Test//(dependsOnGroups = {"PerformanceTests.run"})
     public void shouldAddInvoicesIn10Threads() {
 
         final ExecutorService newFixedThreadPool = Executors.newFixedThreadPool(10);
-        for (int i = 0; i <10; i++) {
+        for (int i = 0; i < 10; i++) {
             newFixedThreadPool.execute(new PerformanceTests());
         }
         newFixedThreadPool.shutdown();
@@ -42,10 +59,87 @@ public class PerformanceTestsRunner extends PerformanceTests{
     }
 
     @Test
-    public void shouldCheckAllID() {
+    public void shouldCorrectlyAddAndGetInvoiceByIdInThreads() {
+        Runnable test = new Runnable() {
+            @Override
+            public void run() {
+                validInputTests.shouldCorrectlyAddAndGetInvoiceById();
+            }
+        };
+        final ExecutorService newFixedThreadPool = Executors.newFixedThreadPool(THREADS_NUMBER);
+        for (int i = 0; i < THREADS_NUMBER; i++) {
+            newFixedThreadPool.submit(test);
+        }
+        newFixedThreadPool.shutdown();
+        try {
+            newFixedThreadPool.awaitTermination(10, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        newFixedThreadPool.shutdown();
     }
 
     @Test
-    public void shouldCheckIfAllInvoicesAreAdded() {
+    public void shouldAddSeveralInvoicesAndReturnCorrectMessageInThreads() {
+        Runnable test = new Runnable() {
+            @Override
+            public void run() {
+                validInputTests.shouldAddSeveralInvoicesAndReturnCorrectMessage();
+            }
+        };
+        final ExecutorService newFixedThreadPool = Executors.newFixedThreadPool(THREADS_NUMBER);
+        for (int i = 0; i < THREADS_NUMBER; i++) {
+            newFixedThreadPool.submit(test);
+        }
+        newFixedThreadPool.shutdown();
+        try {
+            newFixedThreadPool.awaitTermination(10, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        newFixedThreadPool.shutdown();
+    }
+
+    @Test
+    public void shouldAdshouldCorrectlyDeleteInvoiceByIdInThreads() {
+        Runnable test = new Runnable() {
+            @Override
+            public void run() {
+                validInputTests.shouldCorrectlyDeleteInvoiceById();
+            }
+        };
+        final ExecutorService newFixedThreadPool = Executors.newFixedThreadPool(THREADS_NUMBER);
+        for (int i = 0; i < THREADS_NUMBER; i++) {
+            newFixedThreadPool.submit(test);
+        }
+        newFixedThreadPool.shutdown();
+        try {
+            newFixedThreadPool.awaitTermination(10, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        newFixedThreadPool.shutdown();
+    }
+
+    @Test
+    public void shouldCorrectlyUpdateInvoiceInThreads() {
+        Runnable test = new Runnable() {
+            @Override
+            public void run() {
+                validInputTests.shouldCorrectlyUpdateInvoice();
+            }
+        };
+        final ExecutorService newFixedThreadPool = Executors.newFixedThreadPool(THREADS_NUMBER);
+        for (int i = 0; i < THREADS_NUMBER; i++) {
+            newFixedThreadPool.submit(test);
+        }
+        newFixedThreadPool.shutdown();
+        try {
+            newFixedThreadPool.awaitTermination(10, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        newFixedThreadPool.shutdown();
     }
 }
+
