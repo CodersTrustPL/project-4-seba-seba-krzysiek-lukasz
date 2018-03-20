6cdcae34f5bc0ac508a7ac26c5129b09eef2650a
SebastianPal <spalewski@gmail.com>
2018-03-20 22:17:42 +0100

    Content verification added

--- e2e/src/test/java/pl/coderstrust/e2e/performanceTests/PerformanceTests.java
+++ e2e/src/test/java/pl/coderstrust/e2e/performanceTests/PerformanceTests.java
@@ -6,14 +6,20 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pl.coderstrust.e2e.TestsConfiguration;
 import pl.coderstrust.e2e.ValidInputTests;
+import pl.coderstrust.e2e.model.Invoice;
 import pl.coderstrust.e2e.testHelpers.ObjectMapperHelper;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-public class PerformanceTests extends ValidInputTests{
+import static io.restassured.RestAssured.given;
+
+public class PerformanceTests extends ValidInputTests {
 
     private TestsConfiguration config = new TestsConfiguration();
     private ObjectMapperHelper mapper = new ObjectMapperHelper();
@@ -30,12 +36,7 @@ public class PerformanceTests extends ValidInputTests{
 
     @Test(dependsOnGroups = {"ValidInputTests.shouldCorrectlyAddAndGetInvoiceById"})
     public void shouldCorrectlyAddAndGetInvoiceByIdInThreads() {
-        Runnable test = new Runnable() {
-            @Override
-            public void run() {
-                validInputTests.shouldCorrectlyAddAndGetInvoiceById();
-            }
-        };
+        Runnable test = () -> validInputTests.shouldCorrectlyAddAndGetInvoiceById();
         final ExecutorService newFixedThreadPool = Executors.newFixedThreadPool(THREADS_NUMBER);
         for (int i = 0; i < THREADS_NUMBER; i++) {
             newFixedThreadPool.submit(test);
@@ -52,12 +53,7 @@ public class PerformanceTests extends ValidInputTests{
 
     @Test(dependsOnGroups = {"ValidInputTests.shouldAddSeveralInvoicesAndReturnCorrectMessage"})
     public void shouldAddSeveralInvoicesAndReturnCorrectMessageInThreads() {
-        Runnable test = new Runnable() {
-            @Override
-            public void run() {
-                validInputTests.shouldAddSeveralInvoicesAndReturnCorrectMessage();
-            }
-        };
+        Runnable test = () -> validInputTests.shouldAddSeveralInvoicesAndReturnCorrectMessage();
         final ExecutorService newFixedThreadPool = Executors.newFixedThreadPool(THREADS_NUMBER);
         for (int i = 0; i < THREADS_NUMBER; i++) {
             newFixedThreadPool.submit(test);
@@ -74,12 +70,7 @@ public class PerformanceTests extends ValidInputTests{
 
     @Test(dependsOnGroups = {"ValidInputTests.shouldCorrectlyDeleteInvoiceById"})
     public void shouldCorrectlyDeleteInvoiceByIdInThreads() {
-        Runnable test = new Runnable() {
-            @Override
-            public void run() {
-                validInputTests.shouldCorrectlyDeleteInvoiceById();
-            }
-        };
+        Runnable test = () -> validInputTests.shouldCorrectlyDeleteInvoiceById();
         final ExecutorService newFixedThreadPool = Executors.newFixedThreadPool(THREADS_NUMBER);
         for (int i = 0; i < THREADS_NUMBER; i++) {
             newFixedThreadPool.submit(test);
@@ -96,12 +87,7 @@ public class PerformanceTests extends ValidInputTests{
 
     @Test(dependsOnGroups = {"ValidInputTests.shouldCorrectlyUpdateInvoice"})
     public void shouldCorrectlyUpdateInvoiceInThreads() {
-        Runnable test = new Runnable() {
-            @Override
-            public void run() {
-                validInputTests.shouldCorrectlyUpdateInvoice();
-            }
-        };
+        Runnable test = () -> validInputTests.shouldCorrectlyUpdateInvoice();
         final ExecutorService newFixedThreadPool = Executors.newFixedThreadPool(THREADS_NUMBER);
         for (int i = 0; i < THREADS_NUMBER; i++) {
             newFixedThreadPool.submit(test);
@@ -118,12 +104,7 @@ public class PerformanceTests extends ValidInputTests{
 
     @Test(dataProvider = "validDates", dependsOnGroups = {"ValidInputTests.shouldAddSeveralInvoicesAndFindThemByIssueDate"})
     public void shouldAddSeveralInvoicesAndFindThemByIssueDateInThreads(LocalDate newDate) {
-        Runnable test = new Runnable() {
-            @Override
-            public void run() {
-                validInputTests.shouldAddSeveralInvoicesAndFindThemByIssueDate(newDate);
-            }
-        };
+        Runnable test = () -> validInputTests.shouldAddSeveralInvoicesAndFindThemByIssueDate(newDate);
         final ExecutorService newFixedThreadPool = Executors.newFixedThreadPool(THREADS_NUMBER);
         for (int i = 0; i < THREADS_NUMBER; i++) {
             newFixedThreadPool.submit(test);
@@ -137,6 +118,52 @@ public class PerformanceTests extends ValidInputTests{
         newFixedThreadPool.shutdown();
     }
 
+    @Test
+    public void shouldAddSeveralInvoicesInThreadsAndCheckDatabaseSize() {
+        int databaseStartSize = getDatabaseSize();
+        Runnable test = () -> validInputTests.shouldAddSeveralInvoicesAndReturnCorrectMessage();
+        final ExecutorService newFixedThreadPool = Executors.newFixedThreadPool(THREADS_NUMBER);
+        for (int i = 0; i < THREADS_NUMBER; i++) {
+            newFixedThreadPool.submit(test);
+        }
+        newFixedThreadPool.shutdown();
+        try {
+            newFixedThreadPool.awaitTermination(10, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        newFixedThreadPool.shutdown();
+
+        int databaseEndSize = getDatabaseSize();
+        int expectedDatabaseSize = databaseStartSize + 50;
+        org.testng.Assert.assertEquals(expectedDatabaseSize, databaseEndSize);
+    }
+
+    @Test
+    public void shouldCheckDatabaseSizeAndNumberOfIds() {
+        int databaseSize = getDatabaseSize();
+        String path = config.getBaseUri() + config.getBasePort();
+        String response = given()
+                .body(path)
+                .get("")
+                .body().print();
+        List<Invoice> invoicesFromDatabase = mapper.toInvoiceList(response);
+        Set ids = new TreeSet();
+        for (Invoice invoice : invoicesFromDatabase) {
+            ids.add(invoice.getId());
+        }
+        int idsCount = ids.size();
+        org.testng.Assert.assertEquals(databaseSize, idsCount);
+    }
+
+    public int getDatabaseSize() {
+        String path = config.getBaseUri() + config.getBasePort();
+        String response = given()
+                .body(path)
+                .get("")
+                .body().print();
+        return mapper.toInvoiceList(response).size();
+    }
 
     @DataProvider(name = "validDates")
     Object[] validDatesProvider() {
