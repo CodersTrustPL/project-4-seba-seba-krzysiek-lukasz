0dd369f50c315c020b84a3b3ab70aeeda714d328
SebastianPal <spalewski@gmail.com>
2018-04-09 00:42:05 +0200

    MaxID query in InvoiceRepository

--- src/main/java/pl/coderstrust/database/hibernate/InvoiceRepository.java
+++ src/main/java/pl/coderstrust/database/hibernate/InvoiceRepository.java
@@ -1,9 +1,12 @@
 package pl.coderstrust.database.hibernate;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.coderstrust.model.Invoice;
 
 @Repository
 interface InvoiceRepository<T extends Invoice> extends JpaRepository<T, Long> {
+  @Query("SELECT coalesce(max(i.id), 0) FROM Invoice i")
+  Long getMaxId();
 }
